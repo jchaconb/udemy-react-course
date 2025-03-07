e27b060cd287a4407237bd74b6dec8f334c231ca
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-07 09:05:09 -0600

    Render root component with Strict Mode in React

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
