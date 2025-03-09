b14fdd8cdfd7dfb42cc1a85ace51d997325060b3
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-09 11:30:41 -0600

    Initialize React project for 'Travel List'

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -11,11 +11,11 @@ function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step !== 1) setStep(step - 1);
+    if (step !== 1) setStep(step => step - 1);
   }
 
   function handleNext() {
-    if (step !== 3) setStep(step + 1);
+    if (step !== 3) setStep(step => step + 1);
   }
 
   return (
@@ -23,7 +23,7 @@ function App() {
       <button
         className="close"
         onClick={() => {
-          setIsOpen(!isOpen);
+          setIsOpen(isOpen => !isOpen);
         }}
       >
         &times;
