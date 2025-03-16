c326dc580723768f14f957fe8fb3a6425e0e9253
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-16 14:57:05 -0600

    Handle errors to improve reliability

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -55,17 +55,30 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
   const query = 'interstellar';
 
   useEffect(function () {
     async function fetchMovies() {
-      setIsLoading(true);
-      const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`;
-      const res = await fetch(URL);
-      const data = await res.json();
+      try {
+        setIsLoading(true);
+        const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`;
+        const res = await fetch(URL);
 
-      setMovies(data.Search);
-      setIsLoading(false);
+        if (!res.ok)
+          throw new Error('Something went wrong with fetching movies');
+
+        const data = await res.json();
+
+        if (data.Response === 'False') throw new Error('Movie not found');
+
+        setMovies(data.Search);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchMovies();
   }, []);
@@ -78,7 +91,12 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main movies={movies}>
-        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+        <Box>
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
+        </Box>
         <Box>
           <>
             <WatchedSummary watched={watched} />
@@ -87,6 +105,15 @@ export default function App() {
         </Box>
       </Main>
     </>
+  );
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>⛔️</span>
+      {` ${message}`}
+    </p>
   );
 }
 
