25a3474a0530e69b037f59fdda27c3fb2786b564
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-20 09:29:34 -0600

    Implement status handling for loading, error, and ready states

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
 
 const API_QUESTIONS_URL = 'http://localhost:8000/questions';
 
@@ -22,7 +25,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch(API_QUESTIONS_URL)
@@ -36,9 +41,11 @@ export default function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
 }
+
