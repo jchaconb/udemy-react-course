e3b505339d42de1f1382cdb9727a7995f46782fd
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-21 09:46:22 -0600

    Implement timer setup functionality

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -10,8 +10,13 @@ import Question from './components/Question';
 import NextButton from './components/NextButton';
 import Progress from './components/Progress';
 import FinishScreen from './components/FinishScreen';
+import Timer from './components/Timer';
+import Footer from './components/Footer';
+
+import quizData from './questions.json';
 
 const API_QUESTIONS_URL = 'http://localhost:8000/questions';
+const SECONDS_PER_QUESTION = 30;
 
 // status => 'loading', 'error', 'ready', 'active', 'finished'
 const initialState = {
@@ -21,6 +26,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -30,7 +36,11 @@ function reducer(state, action) {
     case 'dataFailed':
       return { ...state, status: 'error' };
     case 'start':
-      return { ...state, status: 'active' };
+      return {
+        ...state,
+        status: 'active',
+        secondsRemaining: state.questions.length * SECONDS_PER_QUESTION,
+      };
     case 'newAnswer':
       const question = state.questions.at(state.index);
 
@@ -58,23 +68,32 @@ function reducer(state, action) {
         status: 'ready',
         highscore: state.highscore,
       };
+    case 'tick':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status,
+      };
     default:
       throw new Error('Action unknown');
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((acc, cur) => acc + cur.points, 0);
 
   useEffect(function () {
-    fetch(API_QUESTIONS_URL)
-      .then(res => res.json())
-      .then(data => dispatch({ type: 'dataReceived', payload: data }))
-      .catch(err => dispatch({ type: 'dataFailed' }));
+    dispatch({ type: 'dataReceived', payload: quizData.questions });
+    // fetch(API_QUESTIONS_URL)
+    //   .then(res => res.json())
+    //   .then(data => dispatch({ type: 'dataReceived', payload: data }))
+    //   .catch(err => dispatch({ type: 'dataFailed' }));
   }, []);
 
   return (
@@ -101,12 +120,15 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton
-              dispatch={dispatch}
-              answer={answer}
-              index={index}
-              numQuestions={numQuestions}
-            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
         )}
         {status === 'finished' && (
