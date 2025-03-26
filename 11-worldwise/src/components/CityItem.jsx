823fb85fd190bac6bf4f5d70b5d8a4ecaf82d5fc
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-25 20:00:24 -0600

    Finish City View

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import styles from './CityItem.module.css';
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = date =>
   new Intl.DateTimeFormat('en', {
@@ -18,9 +19,16 @@ function CityItem({ city }) {
     position: { lat, lng },
   } = city;
 
+  const { currentCity } = useCities();
+
   return (
     <li>
-      <Link to={`${id}?lat=${lat}&lng=${lng}`} className={styles.cityItem}>
+      <Link
+        to={`${id}?lat=${lat}&lng=${lng}`}
+        className={`${styles.cityItem} ${
+          currentCity.id === id ? styles['cityItem--active'] : ''
+        }`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
--- 11-worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -1,18 +1,19 @@
 import { createContext, useEffect, useState, useContext } from 'react';
 
-const BASE_URL = 'http://localhost:8000/cities';
+const BASE_URL = 'http://localhost:8000';
 
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
       try {
         setIsLoading(true);
-        const res = await fetch(`${BASE_URL}`);
+        const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
 
         setCities(data);
@@ -26,8 +27,22 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+
+      setCurrentCity(data);
+    } catch (err) {
+      console.err(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
