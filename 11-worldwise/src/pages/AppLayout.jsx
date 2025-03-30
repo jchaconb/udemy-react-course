abc09ff9d528708c83afb64cdb0fe63a20353c3f
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-29 21:23:02 -0600

    Add Fake Authentication to protect a route

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,8 +1,8 @@
 import Sidebar from '../components/Sidebar';
 import Map from '../components/Map';
+import User from '../components/User';
 
 import styles from './AppLayout.module.css';
-import User from '../components/User';
 
 function AppLayout() {
   return (
