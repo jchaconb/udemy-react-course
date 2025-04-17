8e92540a6c20a95a06dfeb7db93b24b2211dd147
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-04-17 16:09:50 -0600

    Setting up pages and routes

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -1,12 +1,33 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
+import Dashboard from './pages/Dashboard';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      <Button>Button 1</Button>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
