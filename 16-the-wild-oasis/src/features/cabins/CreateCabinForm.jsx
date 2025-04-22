e9ec00e91a4be01a35049eeb78a81dda099bb3cd
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-04-21 21:08:17 -0600

    Add logic to create a new Cabin

--- 16-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 16-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 import { useForm } from 'react-hook-form';
 
+import toast from 'react-hot-toast';
+
 import Input from '../../ui/Input';
 import Form from '../../ui/Form';
 import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createCabin } from '../../services/apiCabins';
 
 const FormRow = styled.div`
   display: grid;
@@ -45,10 +49,24 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isCreating, mutate } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success('New cabin successfully created');
+      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+      reset();
+    },
+    onError: err => {
+      toast.error(err.message);
+    },
+  });
 
   function onSubmit(data) {
-    console.log(data);
+    mutate(data);
   }
 
   return (
@@ -98,7 +116,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Add cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
