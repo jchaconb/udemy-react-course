2679fe69dc7ab04a4886ade9805185ec5d7e42a6
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-04-28 20:28:37 -0600

    Apply render props pattern

--- 16-the-wild-oasis/src/ui/Table.jsx
+++ 16-the-wild-oasis/src/ui/Table.jsx
@@ -37,9 +37,9 @@ const StyledRow = styled(CommonRow)`
   }
 `;
 
-// const StyledBody = styled.section`
-//   margin: 0.4rem 0;
-// `;
+const StyledBody = styled.section`
+  margin: 0.4rem 0;
+`;
 
 // const Footer = styled.footer`
 //   background-color: var(--color-grey-50);
@@ -53,12 +53,12 @@ const StyledRow = styled(CommonRow)`
 //   }
 // `;
 
-// const Empty = styled.p`
-//   font-size: 1.6rem;
-//   font-weight: 500;
-//   text-align: center;
-//   margin: 2.4rem;
-// `;
+const Empty = styled.p`
+  font-size: 1.6rem;
+  font-weight: 500;
+  text-align: center;
+  margin: 2.4rem;
+`;
 
 const TableContext = createContext();
 
@@ -88,12 +88,16 @@ function Row({ children }) {
   );
 }
 
-// function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
+
 // function Footer({ children }) {}
 
 Table.Header = Header;
 Table.Row = Row;
-// Table.Body = Body;
+Table.Body = Body;
 // Table.Footer = Footer;
 
 export default Table;
