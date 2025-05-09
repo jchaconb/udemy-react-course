fcab9f727b1cc9d8f5c5f1ecf2048d879aa9cff7
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-05-08 18:23:58 -0600

    Add functionality to filter cabins

--- 16-the-wild-oasis/src/pages/Cabins.jsx
+++ 16-the-wild-oasis/src/pages/Cabins.jsx
@@ -2,13 +2,14 @@ import CabinTable from '../features/cabins/CabinTable';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import AddCabin from '../features/cabins/addCabin';
+import CabinTableOperatios from '../features/cabins/CabinTableOperatios';
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperatios />
       </Row>
 
       <Row>
