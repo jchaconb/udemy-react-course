b5e6db699150a9a8a9505a378d7c46405468f5f4
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-05-09 20:41:18 -0600

    Filter 'bookings' from API-Side filtering

--- 16-the-wild-oasis/src/features/bookings/useBookings.js
+++ 16-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,13 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
-  const { isLoading, data: bookings, error } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
-  console.log(`here: ${bookings}`)
+  console.log(`here: ${bookings}`);
 
   return { isLoading, bookings, error };
 }
