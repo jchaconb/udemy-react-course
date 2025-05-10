3d94e9ce94f5a0cbaecda5b69ea3c4b2cb20afc4
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-05-10 13:55:03 -0600

    Sort 'bookings' from API-Side sorting

--- 16-the-wild-oasis/src/services/apiBookings.js
+++ 16-the-wild-oasis/src/services/apiBookings.js
@@ -11,8 +11,14 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   // Filter
-  if (filter !== null)
-    query = query[filter.method || 'eq'](filter.field, filter.value);
+  if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
+
+  // Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === 'asc',
+    });
+  
 
   const { data, error } = await query;
 
