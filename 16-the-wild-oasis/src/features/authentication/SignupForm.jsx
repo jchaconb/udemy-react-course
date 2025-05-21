f9490e54601622ea919c189d177972fbd12bffc7
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-05-21 13:31:40 -0600

    Add user sign-up logic with  API integration

--- 16-the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ 16-the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -3,13 +3,15 @@ import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import { useSignup } from './useSignup';
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    signup({ fullName, email, password }, { onSettled: () => reset() });
   }
 
   return (
@@ -18,6 +20,7 @@ function SignupForm() {
         <Input
           type="text"
           id="fullName"
+          disabled={isLoading}
           {...register('fullName', { required: 'This field is required' })}
         />
       </FormRow>
@@ -26,6 +29,7 @@ function SignupForm() {
         <Input
           type="email"
           id="email"
+          disabled={isLoading}
           {...register('email', {
             required: 'This field is required',
             pattern: {
@@ -43,6 +47,7 @@ function SignupForm() {
         <Input
           type="password"
           id="password"
+          disabled={isLoading}
           {...register('password', {
             required: 'This field is required',
             minLength: {
@@ -57,6 +62,7 @@ function SignupForm() {
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isLoading}
           {...register('passwordConfirm', {
             required: 'This field is required',
             validate: value =>
@@ -67,7 +73,7 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
         <Button>Create new user</Button>
