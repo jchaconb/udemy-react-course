314a01db662ff3678c1c0995956d97b6e643d59c
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-05-23 09:50:09 -0600

    Implement logic update user data and password

--- 16-the-wild-oasis/src/features/authentication/UpdatePasswordForm.jsx
+++ 16-the-wild-oasis/src/features/authentication/UpdatePasswordForm.jsx
@@ -19,7 +19,7 @@ function UpdatePasswordForm() {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow
-        label="Password (min 8 characters)"
+        label="New password (min 8 chars)"
         error={errors?.password?.message}
       >
         <Input
@@ -27,11 +27,11 @@ function UpdatePasswordForm() {
           id="password"
           autoComplete="current-password"
           disabled={isUpdating}
-          {...register("password", {
-            required: "This field is required",
+          {...register('password', {
+            required: 'This field is required',
             minLength: {
               value: 8,
-              message: "Password needs a minimum of 8 characters",
+              message: 'Password needs a minimum of 8 characters',
             },
           })}
         />
@@ -46,10 +46,10 @@ function UpdatePasswordForm() {
           autoComplete="new-password"
           id="passwordConfirm"
           disabled={isUpdating}
-          {...register("passwordConfirm", {
-            required: "This field is required",
-            validate: (value) =>
-              getValues().password === value || "Passwords need to match",
+          {...register('passwordConfirm', {
+            required: 'This field is required',
+            validate: value =>
+              getValues().password === value || 'Passwords need to match',
           })}
         />
       </FormRow>
