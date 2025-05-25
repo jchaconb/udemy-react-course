34634955c36d3854177a3b8edead21e127a40bdf
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-05-24 18:48:32 -0600

    Add statistics calculation and display to dashboard

--- 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,10 @@
 import { useRecentBookings } from './useRecentBookings';
 import { useRecentStays } from './useRecentStays';
 import Spinner from '../../ui/Spinner';
+import { useCabins } from '../cabins/useCabins';
 
 import styled from 'styled-components';
+import Stats from './Stats';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -13,17 +15,26 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
 
-  console.log(stays, confirmedStays);
+  console.log(stays);
 
-  if (isLoading1 || isLoading2) return <Spinner />;
-
-  console.log(bookings);
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Todays activity</div>
       <div>Chart stay duration</div>
       <div>Chart sales</div>
